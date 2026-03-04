8d8bfd4f600b47ee91b51cd3eb3a32586911129d
knw2020-glitch <ksnaw2020@gmail.com>
2026-03-04 20:23:44 +0100

    lekcja1.py

---
+++ lekcja1.py
@@ -0,0 +1,35 @@
+print ("cześć")
+
+x=12.12
+y=20.20
+print(x+y)
+
+
+
+z=12.12
+print(type(z))
+
+
+
+
+a="kuba"
+b="ma"
+c=13
+d="lat"
+
+print (a,b,c,d)
+
+a=58
+b="dolars"
+
+print(a,b)
+
+a=58
+b=10.5
+c=2
+d=a+b+c
+print(d)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
